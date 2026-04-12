006284fd12ed1dd5cb6fee9cb00cf3cc80ed8008
donut-hole0 <alanthomasat123@gmail.com>
2026-04-11 22:42:56 -0700

    keep resizing

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <nav>
   <a href="/" class="nav-logo">
-  <img src="/NextGen_Ciruitry_Logo.png" alt="NextGen Circuitry" height="53" />
+  <img src="/NextGen_Ciruitry_Logo.png" alt="NextGen Circuitry" height="55" />
 </a>
 
   <ul>
@@ -61,7 +61,7 @@
   }
 
   .nav-logo img {
-  height: 53px;
+  height: 55px;
   width: auto;
 }
 
